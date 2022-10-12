7ba4e323d94f1cc984a4829bbbe81727411d37c0
IvanRodriguez09 <ivan.rodriguez@mercadolibre.com.co>
2022-10-12 08:28:54 -0500

    Upload the go web module

---
+++ go-web/go-web-5/cmd/server/handler/product.go
@@ -0,0 +1,202 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	products "github.com/IvanRodriguez09/backpack-bcgow6-ivan-rodriguez/go-web/go-web-5/internal/products/service"
+	"github.com/IvanRodriguez09/backpack-bcgow6-ivan-rodriguez/go-web/go-web-5/pkg/web"
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	Name      string  `json:"name"`
+	Color     string  `json:"color"`
+	Price     float64 `json:"price"`
+	Stock     int     `json:"stock"`
+	Code      string  `json:"code"`
+	Published bool    `json:"published"`
+}
+
+type Product struct {
+	service products.Service
+}
+
+func NewProduct(p products.Service) *Product {
+	return &Product{
+		service: p,
+	}
+}
+
+func (c *Product) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		pList, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, pList, ""))
+	}
+}
+
+func (c *Product) GetById() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		p, err := c.service.GetById(id)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, p, ""))
+	}
+}
+
+func (c *Product) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "Invalid id"))
+			return
+		}
+		var req request
+		if err := ctx.ShouldBind(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+		if field, ok := CheckFields(&req); !ok {
+			msg := fmt.Sprintf("Missing field %v", field)
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, msg))
+			return
+		}
+		p, err := c.service.Update(id, req.Name, req.Color, req.Code, req.Price, req.Stock, req.Published)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, p, ""))
+	}
+}
+
+func (c *Product) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		var req request
+		if err := ctx.ShouldBind(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+		if field, ok := CheckFields(&req); !ok {
+			msg := fmt.Sprintf("Missing field %v", field)
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, msg))
+			return
+		}
+		p, err := c.service.Store(req.Name, req.Color, req.Code, req.Price, req.Stock, req.Published)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, p, ""))
+	}
+}
+
+func (c *Product) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "Invalid id"))
+			return
+		}
+		err = c.service.Delete(id)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, "Success", ""))
+	}
+}
+
+func (c *Product) UpdateNameAndPrice() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Invalid token"))
+			return
+		}
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "Invalid id"))
+			return
+		}
+		var req request
+		if err := ctx.ShouldBind(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+		p, err := c.service.UpdateNameAndPrice(id, req.Name, req.Price)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, p, ""))
+	}
+}
+
+func CheckFields(req *request) (field string, ok bool) {
+	if req.Name == "" {
+		ok = false
+		field = "Name"
+		return
+	} else if req.Color == "" {
+		ok = false
+		field = "Color"
+		return
+	} else if req.Price == 0 {
+		ok = false
+		field = "Price"
+		return
+	} else if req.Stock == 0 {
+		ok = false
+		field = "Stock"
+		return
+	} else if req.Code == "" {
+		ok = false
+		field = "Code"
+		return
+	} else if !req.Published {
+		ok = false
+		field = "Published"
+		return
+	}
+	ok = true
+	return
+}
---
+++ go-web/go-web-5/internal/products/repository/repository.go
@@ -0,0 +1,132 @@
+package products
+
+import (
+	"fmt"
+	"time"
+
+	store "github.com/IvanRodriguez09/backpack-bcgow6-ivan-rodriguez/go-web/go-web-5/pkg/store"
+)
+
+type Product struct {
+	Id           int       `json:"id"`
+	Name         string    `json:"name" binding:"required"`
+	Color        string    `json:"color"`
+	Price        float64   `json:"price" binding:"required"`
+	Stock        int       `json:"stock" binding:"required"`
+	Code         string    `json:"code"`
+	Published    bool      `json:"published"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+var pList []Product
+var lastId int
+
+type Repository interface {
+	GetAll() ([]Product, error)
+	GetById(id int) (Product, error)
+	Store(id int, name, color, code string, price float64, stock int, published bool) (Product, error)
+	GetLastId() (int, error)
+	Update(id int, name, color, code string, price float64, stock int, published bool) (Product, error)
+	UpdateNameAndPrice(id int, name string, price float64) (Product, error)
+	Delete(id int) error
+}
+
+type repository struct {
+	db store.Store
+}
+
+func NewRepository(db store.Store) Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+func (r *repository) Store(id int, name, color, code string, price float64, stock int, published bool) (Product, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	p := Product{id, name, color, price, stock, code, published, time.Now()}
+	pList = append(pList, p)
+	if err := r.db.Write(pList); err != nil {
+		return Product{}, err
+	}
+	return p, nil
+}
+
+func (r *repository) GetAll() ([]Product, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	return pList, nil
+}
+
+func (r *repository) GetById(id int) (Product, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	for _, p := range pList {
+		if p.Id == id {
+			return p, nil
+		}
+	}
+	return Product{}, fmt.Errorf("Product not found")
+}
+
+func (r *repository) GetLastId() (int, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	if err := r.db.Read(&pList); err != nil {
+		return 0, err
+	}
+	if len(pList) == 0 {
+		return 0, nil
+	}
+	return pList[len(pList)-1].Id, nil
+}
+
+func (r *repository) Update(id int, name, color, code string, price float64, stock int, published bool) (Product, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	p, err := r.GetById(id)
+	if err != nil {
+		return Product{}, err
+	}
+	p.Name = name
+	p.Color = color
+	p.Code = code
+	p.Price = price
+	p.Stock = stock
+	p.Published = published
+	pList[id-1] = p
+	if err := r.db.Write(pList); err != nil {
+		return Product{}, err
+	}
+	return p, nil
+}
+
+func (r *repository) UpdateNameAndPrice(id int, name string, price float64) (Product, error) {
+	var pList []Product
+	r.db.Read(&pList)
+	p, err := r.GetById(id)
+	if err != nil {
+		return Product{}, err
+	}
+	p.Name = name
+	p.Price = price
+	pList[id-1] = p
+	if err := r.db.Write(pList); err != nil {
+		return Product{}, err
+	}
+	return p, nil
+}
+
+func (r *repository) Delete(id int) error {
+	var pList []Product
+	r.db.Read(&pList)
+	_, err := r.GetById(id)
+	if err != nil {
+		return err
+	}
+	pList = append(pList[:id-1], pList[id:]...)
+	if err := r.db.Write(pList); err != nil {
+		return err
+	}
+	return nil
+}
